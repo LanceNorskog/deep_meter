55cd108e9e9775f13f9eacf648c93e7eebd0fe63
Lance Norskog <goksron@yahoo.com>
2018-09-28 23:18:11 -0700

    classifies iambic_pentameter

--- classify.py
+++ classify.py
@@ -1,11 +1,15 @@
 
+from __future__ import absolute_import, division, unicode_literals
+
+import sys
 import cmudict
 import meter
+from segtok.tokenizer import space_tokenizer
 
 prefix = "test1."
 (syllables, stresses) = cmudict.load_syllables(True)
 
-print(stresses['and'])
+#print(stresses['and'])
 
 # for all meters
 #  create output file
@@ -14,27 +18,35 @@ for name in meter.meters.keys():
   outputs[name] = open(prefix + name, "w")
 
 # read each line
-line = "Houses and rooms are full of perfumes and beets"
-words = ["houses", "and", "rooms", "full", "of", "perfumes", "and", "beets"]
-stressarray = []
-for word in words:
-  stress = stresses[word]
-  if stress == None:
-    stressarray = None
-    break
-  else:
-    print("{0},{1}".format(word, stress))
-    s = ""
-    for st in stress:
-      s = s + st
-    stressarray.append(s)
-
-print(stressarray)
-
-guesses = meter.meter_loose(stressarray)
-print(guesses)
-
+for line in sys.stdin:
+  #line = "Houses and rooms full of perfumes and beets"
+  #words = ["houses", "and", "rooms", "full", "of", "perfumes", "and", "beets"]
+  words = space_tokenizer(line)
+  #print(words)
+  stressarray = []
+  for word in words:
+    word = word.lower()
+    #print(word)
+    stress = stresses[word]
+    if stress == None:
+      stressarray = None
+      break
+    else:
+      #print("{0},{1}".format(word, stress))
+      s = ""
+      for st in stress:
+        s = s + st
+      stressarray.append(s)
+  #print(stressarray)
+  guesses = meter.meter_loose(stressarray)
+  #print(guesses)
 # for each returned meter
 #  append line to meter file
-
-#for name in meter.meter(
+  for guess in guesses:
+    #f = outputs[guess]
+    #print(type(f))
+    #f.write(line)
+    outputs[guess].write(line)
+  break
+for (name, f) in outputs.items():
+  f.close()
