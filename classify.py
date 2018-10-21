3c4036f62f491c9817765eb68d45a85e405deb7c
Lance Norskog <goksron@yahoo.com>
2018-10-21 14:52:29 -0700

    fiddling

--- classify.py
+++ classify.py
@@ -1,3 +1,5 @@
+TODO - change cmudict.get_syllables to get variant syllables that have same meter, add to emitted lines as separate lines so that randomizing works
+TODO - write randomizer, train/dev/test split
 
 from __future__ import absolute_import, division, unicode_literals
 
