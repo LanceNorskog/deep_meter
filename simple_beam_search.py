08e9181ffcf277c38b5e67067333f3a0fb5d7921
Lance Norskog <goksron@yahoo.com>
2018-11-29 21:42:18 -0800

    experiment

--- simple_beam_search.py
+++ simple_beam_search.py
@@ -18,7 +18,24 @@ def beam_search_decoder(data, k):
         for i in range(len(sequences)):
             seq, score = sequences[i]
             for j in range(len(row)):
-                candidate = [seq + [j], score * row[j]]
+                candidate = [seq + [j], score * (1.0 + log(row[j])]
+                all_candidates.append(candidate)
+        # order all candidates by score
+        ordered = sorted(all_candidates, key=lambda tup:tup[1])
+        # select k best
+        sequences = ordered[-k:]
+    return sequences
+
+def beam_search_decoder2(data, k):
+    sequences = [[list(), 1.0]]
+    # walk over each step in sequence
+    for row in data:
+        all_candidates = list()
+        # expand each current candidate
+        for i in range(len(sequences)):
+            seq, score = sequences[i]
+            for j in range(len(row)):
+                candidate = [seq + [j], score + (1.0 + log(row[j])]
                 all_candidates.append(candidate)
         # order all candidates by score
         ordered = sorted(all_candidates, key=lambda tup:tup[1])
