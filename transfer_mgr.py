393d410d4ee5aa36df5cc0a4caa2373b92d182c9
Lance Norskog <goksron@yahoo.com>
2018-12-01 19:09:20 -0800

    refactoring

--- transfer_mgr.py
+++ transfer_mgr.py
@@ -37,7 +37,7 @@ def new_squid_model(embed_size, num_symbols, num_syllables, optimizer='adam', dr
       squid = layers.Dropout(dropout)(dense)
       squid = layers.Dense(256, activation='relu', name='Squid'+str(i))(squid)
       squid = layers.Dropout(dropout)(squid)
-      name = 'Flatout'+str(i)
+      name = 'Onehot'+str(i)
       pred_array.append(layers.Dense(num_syllables, activation='softmax', name=name)(squid))
       loss_array.append('categorical_crossentropy')
       names_array.append(name)
@@ -49,7 +49,6 @@ def new_squid_model(embed_size, num_symbols, num_syllables, optimizer='adam', dr
 
 def save_squid_model(model):
     model.save_weights('./model_squid.h5')
-
 
 def load_squid_model(model):
     model.load_weights('./model_squid.h5')  
@@ -70,5 +69,65 @@ def pop_layer(model):
     else:
         model.layers[-1].outbound_nodes = []
         model.outputs = [model.layers[-1].output]
-    model.built = False
+    #model.built = False
 
+# above model has dropout, squidbrain, dropout, onehot for each symbol
+def remove_squid(model, num_symbols):
+    for i in range(r * num_sumbols):
+        pop_layer(model)
+
+def create_weighted_binary_crossentropy(zero_weight, one_weight, num_syllables):
+
+    def weighted_binary_crossentropy(y_true, y_pred):
+
+        # Original binary crossentropy (see losses.py):
+        # K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)
+
+        # Calculate the binary crossentropy
+        b_ce = K.binary_crossentropy(y_true, y_pred)
+
+        # Apply the weights
+        weight_vector = y_true * one_weight + (1. - y_true) * zero_weight
+        weighted_b_ce = weight_vector * b_ce
+
+        # Return the mean error
+        return K.mean(weighted_b_ce)
+
+    return weighted_binary_crossentropy
+
+# Single output layer, just soaks up all syllables in multi-label configuration
+# Used for training "transferable knowledge" about outputting to syllables.
+def new_transfer_model(embed_size=512, num_symbols=10, num_syllables=0, optimizer='adam', dropout=0.5):
+    custom_binary_crossentropy = create_weighted_binary_crossentropy(1/(np.sqrt(num_syllables)), 1 - 1/np.sqrt(num_syllables))
+    input_embeddings = layers.Input(shape=(embed_size,), dtype=tf.float32, name='Input')
+    dense_input = layers.Dropout(dropout)(input_embeddings)
+    dense = layers.Dense(1024, activation='relu', name='Human')(dense_input)
+    dense = layers.Dropout(dropout)(dense)
+    dense = layers.Dense(2048, activation='relu', name='Chimp')(dense)
+    dense = layers.Dropout(dropout)(dense)
+    dense = layers.Dense(4096, activation='relu', name='Lizard')(dense)
+    dense = layers.Dropout(dropout)(dense)
+    pred = layers.Dense(num_syllables, activation='sigmoid', name='Onehot')(dense)
+    model = Model(inputs=input_embeddings, outputs=pred_array)
+    model.compile(loss=custom_binary_crossentropy, 
+                  optimizer=optimizer, 
+                  metrics=['binary_crossentropy'])
+    return model
+
+def save_transfer_model(model):
+    model.save_weights('./model_transfer.h5')
+
+def load_transfer_model(model):
+    model.load_weights('./model_transfer.h5')  
+
+def remove_transfer_model():
+    os.remove('./model_transfer.h5')
+
+def new test_model(model, output_size=0):
+    old_in = model.input
+    old_out = K.new_layer(model.layers[-1].output)
+    dense = layers.Dropout(dropout)(old_out)
+    dense = layers.Dense(output_size, activation='sigmoid', name='Test')(dense)
+    model2 = Model(old_in, dense)
+    model2.summary()
+    return model2
